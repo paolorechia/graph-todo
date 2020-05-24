d28b1ce9ad2bcc8796f3ea3c22dedbfd5fa23d0b
Paolo Rechia <paolo@e-bot7.com>
2020-05-23 21:58:01 -0300

    Users mock endpoints

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,7 +3,35 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with user resources\n');
+  res.send('list users\n');
+});
+
+router.post('/', function(req, res, next) {
+  res.send('create new user\n');
+});
+
+router.put('/', function(req, res, next) {
+  res.send('modify user attributes\n');
+});
+
+router.patch('/api_key', function(req, res, next) {
+  res.send('generate new api key for user\n');
+});
+
+router.get('/:id', function(req, res, next) {
+  res.send('respond with specific user\n');
+});
+
+router.delete('/:id', function(req, res, next) {
+  res.send('delete user\n');
+});
+
+router.delete('/api_key/<key_end>', function(req, res, next) {
+  res.send('delete api key ending with substring for user\n');
+});
+
+router.delete('/api_key', function(req, res, next) {
+  res.send('delete all api keys for user\n');
 });
 
 module.exports = router;
