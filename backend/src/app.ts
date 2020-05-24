2b2009baeb6b598151ab44cb5fa6eda9f3febe4f
Paolo Rechia <paolo@e-bot7.com>
2020-05-24 17:19:35 -0300

    Writing API spec for card routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -27,10 +27,10 @@ const options = {
         info: {
             description: 'This is a sample server',
             title: 'Swagger',
-            version: '1.0.0',
+            version: '0.1.0',
         },
         host: 'localhost:3000',
-        basePath: '/v1',
+        basePath: '/v0',
         produces: [
             "application/json",
         ],
--- backend/src/routes/models/card.ts
+++ backend/src/routes/models/card.ts
@@ -1,14 +1,51 @@
+
 /**
- * @typedef Card
+ * @typedef Tag
+ * @property {string} name - tag name
+ */
+
+class TagModel {
+  name: string;
+}
+
+/**
+ * @typedef CardRef 
  * @property {string} shid - short hash id for card
  * @property {string} name.required - Card name
  */
 
-console.log("Reading cards model")
 
-class Card {
+/**
+ * @typedef Card
+ * @property {CardRef} current - current card
+ * @property {Array<Tag>} tags - children cards
+ * @property {Array<CardRef>} children - children cards
+ * @property {Array<CardRef>} parents - parents cards
+ */
+
+
+/**
+ * @typedef TreeNode
+ * @property {CardRef} current - current Ref
+ * @property {Array<Card>} children - children nodes
+ * @property {Array<Card>} parents  - parent nodes
+ */
+
+class CardRef {
   shid: string;
   name: string;
 }
 
-export default Card;
+class CardModel {
+  current: CardRef;
+  tags: Array<TagModel>;
+  children: Array<CardModel>;
+  parents: Array<CardModel>;
+}
+
+
+
+export {
+  CardModel,
+  TagModel
+}
